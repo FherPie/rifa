c2ffbd51269c6a5728e473bd97197aff0b906a39
FherPie <panaonchoatu@hotmail.com>
2022-05-09 13:00:24 -0500

    cambios janathan zapata

---
+++ js/datos.js
@@ -0,0 +1,11 @@
+
+
+
+function myFunction() {
+  let carName = "Volvo";
+  document.getElementById("fecha").innerHTML = "90";
+}
+
+document.getElementById("demo2").innerHTML = typeof carName;
+
+myFunction();
